5ba2b1e84c299bd695e1d9cb1703ef17eef9a139
Dario Zilocchi <dario.zilocchi@gmail.com>
2016-11-16 00:31:26 +0100

    Check ROLE_ADMIN e ROLE_SUPER_ADMIN Verifica ruoli su pagine edit/view utente

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,43 +8,31 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
-class UserController extends Controller{
+class UserController extends Controller {
 
 	/**
 	 * @Route("/user/edit/{id}", name="userEdit")
 	 */
-	public function editAction(Request $request, $id){
-
+	public function editAction(Request $request, $id) {
 		//edit single user
 		//se utente loggato è ROLE_ADMIN allora può modificare se l'oggetto è di tipo ROLE_USER
 		//se utente loggato è ROLE_SUPER_ADMIN allora può modificare tutti gli utenti?
 		//da sistemare
 
-		$userManager = $this->get('fos_user.user_manager');
-
-		$user = $userManager->findUserBy(array('id' => $id));
+		$user = $this->checkUser($id);
+		$roles = $this->checkUserRoles($user);
 
 		$form = $this->createFormBuilder($user)
 			->add('username', TextType::class, array('label' => 'Username utente:'))
 			->add('name', TextType::class, array('label' => 'Nome:'))
 			->add('surname', TextType::class, array('label' => 'Cognome:'))
-			//->add('dueDate', DateType::class)
 			->add('save', SubmitType::class, array('label' => 'Modifica le informazioni sull\'utente'))
 			->getForm();
 
 		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid()) {
-			// $form->getData() holds the submitted values
-			// but, the original `$task` variable has also been updated
 			$user = $form->getData();
-
-			// ... perform some action, such as saving the task to the database
-			// for example, if Task is a Doctrine entity, save it!
-			// $em = $this->getDoctrine()->getManager();
-			// $em->persist($task);
-			// $em->flush();
-
 			$userManager->updateUser($user, true);
 
 			//return $this->redirectToRoute('task_success');
@@ -52,6 +40,7 @@ class UserController extends Controller{
 
 		return $this->render('user/edit.html.twig', array(
 			'user' => $user,
+			'roles' => $user->getRoles(),
 			'form' => $form->createView(),
 			'isSubmitted' => $form->isSubmitted(),
 			'isValid' => $form->isValid()
@@ -61,15 +50,42 @@ class UserController extends Controller{
 	/**
 	 * @Route("/user/view/{id}", name="userView")
 	 */
-	public function viewAction($id){
-
+	public function viewAction($id) {
 		//edit single user
 		//se utente loggato è ROLE_ADMIN allora può modificare se l'oggetto è di tipo ROLE_USER
 		//se utente loggato è ROLE_SUPER_ADMIN allora può modificare tutti gli utenti?
 		//da sistemare
 
+		$user = $this->checkUser($id);
+		$roles = $this->checkUserRoles($user);
+		
+		return $this->render('user/view.html.twig', array('user' => $user));
+	}
+
+	private function checkUser($id) {
 		$userManager = $this->get('fos_user.user_manager');
 		$user = $userManager->findUserBy(array('id' => $id));
-		return $this->render('user/view.html.twig', array('user' => $user));
+
+		if (!$user) {
+			throw $this->createNotFoundException('User not found');
+		}
+
+		return $user;
+	}
+
+	private function checkUserRoles($user) {
+		$loggedUser = $this->getUser();
+		$loggedRoles = $loggedUser->getRoles();
+		
+		$isAdmin = in_array('ROLE_ADMIN', $loggedRoles);
+		$isSuperAdmin = in_array('ROLE_SUPER_ADMIN', $loggedRoles);
+
+		$roles = $user->getRoles();
+
+		if ((in_array('ROLE_ADMIN', $roles) || in_array('ROLE_SUPER_ADMIN', $roles)) && !$isSuperAdmin) {
+			throw new AccessDeniedException('You need to be a super admin to view this page');
+		}
+
+		return $roles;
 	}
 }
